700c27f3993c8ce1efd97c78f7935f31f1ccd36e
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2024-08-20 11:46:41 +0300

    added bg

--- app/page.js
+++ app/page.js
@@ -3,9 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-200 h-screen">
+    <main className="bg-gray-200 h-screen bg-[url('https://www.ivanhoemines.com/wp-content/themes/ivanhoemines/img/home_operations_map_drc.svg?v2)')] bg-cover bg-center ">
+      
+      <div className="bg-mainColor bg-opacity-50 h-full">
 
       <HeroSection/>
+
+      </div>
+
+
 
 
     </main>
