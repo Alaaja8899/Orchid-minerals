3d94268870eb87b0cac82dd9810b13c40fff207c
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2024-08-20 19:38:13 +0300

    video emebed problem fixed

--- components/homeComponents/HeroSection.js
+++ components/homeComponents/HeroSection.js
@@ -14,10 +14,10 @@ const HeroSection = () => {
       
       <div className="relative z-10 flex flex-col items-start justify-center h-full bg-[rgba(0,0,0,0.8)]">
         <div className="text-white text-center gap-4 flex flex-col p-6 h-full my-20">
-          <h1 className='md:text-4xl text-2xl font-bold text-white'>
+          <h1 className='md:text-4xl text-3xl font-bold text-white'>
             Mining with a greater purpose.
           </h1>
-          <p className='font-medium md:text-2xl text-gray-400'>
+          <p className='font-medium text-2xl text-blue-100'>
             We believe that mining, done right, with purpose, vision, and a commitment to environmental and social responsibility, will play a significant role in the world’s transition to clean energy.
           </p>
         </div>
