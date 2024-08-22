69d637b652c4952f594598d854ce69caf90f9ea5
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2024-08-22 20:03:18 +0300

    finally toggled

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,8 @@ function Header() {
   return (
     <div className='bg-mainColor border-b text-white fixed h-[100px] flex items-center justify-center right-0 left-0 top-0 z-20'>
         <div className="w-full container   relative h-full mx-auto p-6 flex items-center justify-between">
-        <span href="" className="brand">
+        <span 
+        href="" className="brand">
             <Link href={'/'}>
             <Image src={'/assets/logo-white.png'} width={200} height={400} />
             </Link>
@@ -23,7 +24,7 @@ function Header() {
 
         <nav className={`${open ? 'flex':' hidden md:flex'} gap-3 items-center  ${open&& 'absolute md:relative'} md:top-0 top-[100%] bg-mainColor left-0 right-0 md:flex-row  flex-col md:h-fit h-dvh   transition duration-300 ease-out px-6`}>
     
-            <span  
+            <span onClick={()=> setOpen(!open)}  
             className={` md:w-fit w-full ${open && 'border-b border-t'} rounded items-center flex gap-2 p-3 hover:border-b hover:border-white hover:text-gray-300`}>
             <Link href={'/what-we-do'}  >
             What we do 
@@ -31,14 +32,16 @@ function Header() {
             {open && <FaArrowRight/>}
             </span>
 
-            <span className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300 `}>
+            <span onClick={()=> setOpen(!open)} 
+            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300 `}>
             <Link href={'/'}  >
             Sustainability 
             </Link>
             {open && <FaArrowRight/>}
             </span>
 
-            <span className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
+            <span onClick={()=> setOpen(!open)}
+            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
 
             <Link href={'/'}  >
             News & Stories 
@@ -46,27 +49,33 @@ function Header() {
             {open && <FaArrowRight/>}
             </span>
 
-            <span className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
+            <span onClick={()=> setOpen(!open)}
+            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
             <Link href={'/'}  >
             Investors 
             </Link>
             {open && <FaArrowRight/>}
             </span>
 
-            <span className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
+            <span onClick={()=> setOpen(!open)}
+            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
             <Link href={'/'}  >
                 Reports 
             </Link>
             {open && <FaArrowRight/>}
             </span>
 
-            <span className='border border-white rounded-full px-2 py-1 md:w-fit w-1/2'>
+            <span 
+            onClick={()=> setOpen(!open)}
+            className='border border-white rounded-full px-2 py-1 md:w-fit w-1/2'>
             <Link href={'/'}  >
             Contact                
             </Link>
             </span>
 
-            <span>
+            <span
+            onClick={()=> setOpen(!open)}
+            >
             <Link href={'/'}  >
             <FaSearch />
             </Link>
