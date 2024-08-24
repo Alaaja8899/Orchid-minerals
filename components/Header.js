7c57c7351d57a212bcde53a145efbce3991fd721
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2024-08-24 10:35:44 +0300

    sus page added

--- components/Header.js
+++ components/Header.js
@@ -2,98 +2,86 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-
 import { FaArrowRight } from "react-icons/fa6";
-
-
-import { FaSearch,FaBars } from "react-icons/fa";
+import { FaSearch, FaBars } from "react-icons/fa";
 import { MdOutlineClose } from "react-icons/md";
 
 function Header() {
-        const [open , setOpen] =useState(false)
+    const [open, setOpen] = useState(false);
 
-  return (
-    <div className='bg-mainColor border-b text-white fixed h-[100px] flex items-center justify-center right-0 left-0 top-0 z-20'>
-        <div className="w-full container   relative h-full mx-auto p-6 flex items-center justify-between">
-        <span 
-        href="" className="brand">
-            <Link href={'/'}>
-            <Image src={'/assets/logo-white.png'} width={200} height={400} />
-            </Link>
-        </span>
+    return (
+        <div className='bg-mainColor border-b text-white fixed h-[100px] flex items-center justify-center right-0 left-0 top-0 z-20'>
+            <div className="w-full container relative h-full mx-auto p-6 flex items-center justify-between">
+                <span className="brand">
+                    <Link href={'/'}>
+                        <Image src={'/assets/logo-white.png'} width={200} height={400} />
+                    </Link>
+                </span>
 
-        <nav className={`${open ? 'flex':' hidden md:flex'} gap-3 items-center  ${open&& 'absolute md:relative'} md:top-0 top-[100%] bg-mainColor left-0 right-0 md:flex-row  flex-col md:h-fit h-dvh   transition duration-300 ease-out px-6`}>
-    
-            <span onClick={()=> setOpen(!open)}  
-            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded items-center flex gap-2 p-3 hover:border-b hover:border-white hover:text-gray-300`}>
-            <Link href={'/what-we-do'}  >
-            What we do 
-            </Link>
-            {open && <FaArrowRight/>}
-            </span>
+                <nav className={`${open ? 'flex' : 'hidden md:flex'} gap-3 items-center ${open && 'absolute md:relative'} md:top-0 top-[100%] bg-mainColor left-0 right-0 md:flex-row flex-col md:h-fit h-dvh transition duration-300 ease-out px-6`}>
+                    <ul className="flex flex-col md:flex-row gap-3 w-full">
+                        <li onClick={() => setOpen(!open)}
+                            className={`md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300`}>
+                            <Link href={'/what-we-do'}>
+                                What we do
+                            </Link>
+                            {open && <FaArrowRight />}
+                        </li>
 
-            <span onClick={()=> setOpen(!open)} 
-            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300 `}>
-            <Link href={'/'}  >
-            Sustainability 
-            </Link>
-            {open && <FaArrowRight/>}
-            </span>
+                        <li onClick={() => setOpen(!open)}
+                            className={`md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300`}>
+                            <Link href={'/'}>
+                                Sustainability
+                            </Link>
+                            {open && <FaArrowRight />}
+                        </li>
 
-            <span onClick={()=> setOpen(!open)}
-            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
+                        <li onClick={() => setOpen(!open)}
+                            className={`md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300`}>
+                            <Link href={'/'}>
+                                News & Stories
+                            </Link>
+                            {open && <FaArrowRight />}
+                        </li>
 
-            <Link href={'/'}  >
-            News & Stories 
-            </Link>
-            {open && <FaArrowRight/>}
-            </span>
+                        <li onClick={() => setOpen(!open)}
+                            className={`md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300`}>
+                            <Link href={'/'}>
+                                Investors
+                            </Link>
+                            {open && <FaArrowRight />}
+                        </li>
 
-            <span onClick={()=> setOpen(!open)}
-            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
-            <Link href={'/'}  >
-            Investors 
-            </Link>
-            {open && <FaArrowRight/>}
-            </span>
+                        <li onClick={() => setOpen(!open)}
+                            className={`md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 items-center p-3 hover:border-b hover:border-white hover:text-gray-300`}>
+                            <Link href={'/'}>
+                                Reports
+                            </Link>
+                            {open && <FaArrowRight />}
+                        </li>
 
-            <span onClick={()=> setOpen(!open)}
-            className={` md:w-fit w-full ${open && 'border-b border-t'} rounded flex gap-2 p-3 items-center hover:border-b hover:border-white hover:text-gray-300`}>
-            <Link href={'/'}  >
-                Reports 
-            </Link>
-            {open && <FaArrowRight/>}
-            </span>
+                        <li onClick={() => setOpen(!open)}
+                            className='border border-white rounded-full px-2 py-1 md:w-fit w-1/2'>
+                            <Link href={'/'}>
+                                Contact
+                            </Link>
+                        </li>
 
-            <span 
-            onClick={()=> setOpen(!open)}
-            className='border border-white rounded-full px-2 py-1 md:w-fit w-1/2'>
-            <Link href={'/'}  >
-            Contact                
-            </Link>
-            </span>
+                        <li onClick={() => setOpen(!open)}>
+                            <Link href={'/'}>
+                                <FaSearch />
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
 
-            <span
-            onClick={()=> setOpen(!open)}
-            >
-            <Link href={'/'}  >
-            <FaSearch />
-            </Link>
-            </span>
-
-        </nav>
-
-
-            <span className='cursor-pointer duration-300  ease-linear md:hidden block'
-            onClick={()=>setOpen(!open)}
-            >
-                {open ? <MdOutlineClose size={32} />:<FaBars size={32}/>}
-            </span>
-
+                <span className='cursor-pointer duration-300 ease-linear md:hidden block'
+                    onClick={() => setOpen(!open)}>
+                    {open ? <MdOutlineClose size={32} /> : <FaBars size={32} />}
+                </span>
+            </div>
         </div>
-
-    </div>
-  )
+    );
 }
 
-export default Header
+export default Header;
